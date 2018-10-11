df0839f345f0e90a3d14fa223cc3f25dcaffa140
asultan00 <asultan00@stuy.edu>
2018-10-11 07:22:03 -0400

    everything works, but the distance functions keep on giving me weird numbers no matter what method I try

---
+++ Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+  /* more methods can go here*/
+  public static void main(String[]args){
+     //point creation
+     Point P1 = new Point(1,1);
+     Point P2 = new Point(2,2);
+     Point P3 = new Point(3,3);
+     //making sure points work
+     System.out.println("--- POINTS ACTIVE ---");
+     System.out.println(P1);
+     System.out.println(P2);
+     System.out.println(P3);
+     System.out.println("--- POINTS ACTIVE ---");
+     System.out.println(Point.distance(P1,P2));
+     System.out.println(P1.distanceTo(P2));
+     System.out.println(Point.distance(P1, P3));
+  }
+ }
--- Point.java
+++ Point.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Point {
      private double x;
      private double y;
@@ -17,5 +18,19 @@ public class Point {
      }
      public double getY(){
           return y;
+     }
+     public String toString() {
+          return "(" + x + " ," + y + ")";
+     }
+     //distance (STATIC) & distanceTo (NONSTATIC)
+     public static double distance(Point p1, Point p2) {
+          double xa = Math.pow((p2.getX() - p1.getX()), 2);
+          double ya = Math.pow((p2.getY() - p1.getY()), 2);
+          return Math.sqrt(xa + ya);
+     }
+     public double distanceTo(Point p2) {
+          double xb = (p2.getX() - x) * (p2.getX() - x);
+          double yb = (p2.getY() - y) * (p2.getY() - y);
+          return Math.sqrt(xb + yb);
      }
 }
